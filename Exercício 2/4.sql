4339138995df06abcd850bc63bea6683e4ca8a7b
Nelson Gomes Neto <ngn@ic.ufal.br>
2017-10-04 00:23:59 -0300

    LoL

--- Exercício 2/4.sql
+++ Exercício 2/4.sql
@@ -1,3 +1,4 @@
 select Medico.nome
 from Medico
-where not exists(select Consulta.crm from Consulta where Medico.crm = Consulta.crm)
+#where not exists(select Consulta.crm from Consulta where Medico.crm = Consulta.crm)
+where Medico.crm not in (select Consulta.crm from Consulta)
